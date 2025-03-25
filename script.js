cc364f9f15ed32567625c958a856a31fde28cfef
YasBorba <yasmin.b0rbalopes@gmail.com>
2025-03-25 10:33:06 -0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Digitação (JS Completo)
+//Controle do menu mobile 
+const menuIcon = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navlist');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+
+    //bloquear o scroll quando o menu esiver aberto
+    document.body.style.overflow = navList.classList.contains('open') ? 'hidden' : 'auto';
+});
+
+//fechar menu ao clicar nos links
+document.querySelectorAll('.navlist a').forEach(link => {
+    link.addEventListener('click' , () => {
+        menuIcon.classList.remove('bx-x');
+        navList.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    });
+});
+
+//Fechar ao rolar a pagina
+window.addEventListener('scroll', () => {
+    if (navList.classList.contains('open')){
+        menuIcon.classList.remove('bx-x');
+        navList.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    }
+});
